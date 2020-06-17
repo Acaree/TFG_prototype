a94ceeb69320d9c89684641ef315e228463c1f8e
Acaree <acampamar@gmail.com>
2020-06-18 01:30:45 +0200

    Project swapped to 2D + movement

--- TFG/Assets/Scripts/Player.cs
+++ TFG/Assets/Scripts/Player.cs
@@ -4,68 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
 
-   public float rotation_speed = 0.0f;
+    Vector2 new_position;
     public float movement_speed = 0.0f;
 
-    Vector3 new_position;
-   Vector3 new_rotation;
-    
-
+    // Start is called before the first frame update
     void Start()
     {
-        Vector3 new_position = Vector3.zero;
-        Vector3 new_rotation = Vector3.zero;
-        
+        new_position = transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        new_position = transform.position;
 
         if (Input.GetKey(KeyCode.W))
         {
-            new_position.z += 0.01f * movement_speed;
-            
+            new_position.y += movement_speed;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            new_position.z -= 0.01f * movement_speed;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            new_position.x += 0.01f * movement_speed;
+            new_position.y -= movement_speed;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            new_position.x -= 0.01f * movement_speed;
+            new_position.x -= movement_speed;
         }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            new_position.x += movement_speed;
+        }
+
 
         transform.position = new_position;
 
-        DoRotation();
-
-    }
-
-
-
-    void DoRotation()
-    {
-
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
-
-        new_rotation.x -= mouseY * rotation_speed;
-        new_rotation.z = 0;
-        new_rotation.y += mouseX * rotation_speed;
-
-        transform.rotation = Quaternion.Euler(new_rotation);
     }
 }
-
-    
