2e9615a3a37fd24c469c8707f5a2c192a82fbb1a
Acaree <acampamar@gmail.com>
2020-06-19 00:38:04 +0200

    Day 14283402457: Stil without understanding unity colliders

---
+++ TFG/Assets/Scripts/UI_Trigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Trigger : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject UI_to_activate;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            UI_to_activate.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            UI_to_activate.SetActive(true);
+        }
+    }
+
+}
