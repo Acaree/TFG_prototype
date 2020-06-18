c518dc4435bbcd5451d00445daff9bab28bc7713
Acaree <acampamar@gmail.com>
2020-06-18 23:23:29 +0200

    Squirell animations

--- TFG/Assets/Scripts/Player.cs
+++ TFG/Assets/Scripts/Player.cs
@@ -18,23 +18,32 @@ public class Player : MonoBehaviour
     void FixedUpdate()
     {
 
+        transform.GetComponent<Animator>().SetBool("Front", false);
+        transform.GetComponent<Animator>().SetBool("Back", false);
+        transform.GetComponent<Animator>().SetBool("Left", false);
+        transform.GetComponent<Animator>().SetBool("Right", false);
+
         if (Input.GetKey(KeyCode.W))
         {
             new_position.y += movement_speed;
+            transform.GetComponent<Animator>().SetBool("Front", true);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
+            transform.GetComponent<Animator>().SetBool("Back", true);
             new_position.y -= movement_speed;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
+            transform.GetComponent<Animator>().SetBool("Left", true);
             new_position.x -= movement_speed;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
+            transform.GetComponent<Animator>().SetBool("Right", true);
             new_position.x += movement_speed;
         }
 
