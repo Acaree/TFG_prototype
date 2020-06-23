74d57952f4ce7d17f88c1a1d699a6e97383b09a5
Acaree <acampamar@gmail.com>
2020-06-23 18:01:21 +0200

    Bunny follow

---
+++ TFG/Assets/Scripts/Follow_BUNNY.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow_BUNNY : MonoBehaviour
+{
+    public bool following = false;
+    public float min_distance = 1.5f;
+    private float distance;
+    public GameObject player;
+    public float speed = 1.0f;
+    private Vector3 new_position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        new_position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        distance = (player.transform.position - transform.position).magnitude;
+
+
+
+        if (distance > min_distance && following)
+        {
+            if (transform.position.x > player.transform.position.x)
+            {
+                new_position.x -= speed;
+            }
+
+            if (transform.position.x < player.transform.position.x)
+            {
+                new_position.x += speed;
+            }
+
+            if (transform.position.y > player.transform.position.y)
+            {
+                new_position.y -= speed;
+            }
+
+            if (transform.position.y < player.transform.position.y)
+            {
+                new_position.y += speed;
+            }
+
+            transform.position = new_position;
+
+        }
+    }
+   
+}
